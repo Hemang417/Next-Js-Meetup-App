61f7a8b2a677515e3d4ccd0d9b788347ca6fab2b
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-06-24 21:44:47 +0530

    Learned
    learned about getStaticProps(), getStaticPaths(), getServerSideProps(), and context

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "49d251d574e8eb12"; }
+/******/ 		__webpack_require__.h = function() { return "c031be3a3edae3e0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/[meetupID]/index.js
+++ pages/[meetupID]/index.js
@@ -10,4 +10,40 @@ function MeetupDetails(){
     )
 }
 
+export async function getStaticPaths(){
+    return {
+        fallback: false,
+        paths: [
+            { 
+                params: {
+                meetupID: 'm1',
+                }, 
+            },
+            { 
+                params: {
+                meetupID: 'm2',
+                }, 
+            },
+        ],
+            
+        };
+    }
+
+export async function getStaticProps(context){
+
+    const meetupId = context.params.meetupID;
+    console.log(meetupId);
+    return {
+        props: {
+            meetupData: {
+                image: "https://cdn.britannica.com/36/162636-050-932C5D49/Colosseum-Rome-Italy.jpg?q=60",
+                title: "First Meetup",
+                id: meetupId,
+                address: "Some City, 12345, Some Neighbourhood",
+                description: "The First Meetup",
+            }
+        }
+    };
+}
+
 export default MeetupDetails;
